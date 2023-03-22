b980939d495205e9090d86fb1644b74bdf43eba4
Madhav Prahlad <prahladmadhav@gmail.com>
2023-03-22 22:16:18 +0530

    Set ejs as the view engine update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8000;
 // use express router
 app.use("/", require("./routes"));
 // set ejs as the view_engine
-app.set("view_engine", "ejs");
+app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.listen(port, (err) => {
