b286b7972b20aee19ba5d747f1d64a602de68114
Madhav Prahlad <prahladmadhav@gmail.com>
2023-03-22 22:57:44 +0530

    Setup express-ejs-layout and create a layout

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
 
+// use express layouts
+app.use(expressLayouts);
 // use express router
 app.use("/", require("./routes"));
 // set ejs as the view_engine
