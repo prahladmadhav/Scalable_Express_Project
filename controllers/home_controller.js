c2c175acf7db24cfbf2100d8b04d88c64bee0854
Madhav Prahlad <prahladmadhav@gmail.com>
2023-03-31 22:54:37 +0530

    Nested Populate and display comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,12 +3,20 @@ const Post = require("../models/post");
 module.exports.home = async (req, res) => {
     let posts = [];
     try {
-        posts = await Post.find({}).populate("user").exec();
+        posts = await Post.find({})
+            .populate("user")
+            .populate({
+                path: "comments",
+                populate: {
+                    path: "user",
+                },
+            })
+            .exec();
     } catch (err) {
         console.log(`Error pulling all Posts: ${err}`);
     }
     return res.render("home", {
         title: "Home",
-        posts: posts
+        posts: posts,
     });
 };
