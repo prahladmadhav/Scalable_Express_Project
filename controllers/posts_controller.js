372503b018055064a3a53483c3bb7dfbbbe38556
Madhav Prahlad <prahladmadhav@gmail.com>
2023-04-01 00:00:36 +0530

    Delete authorized posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 module.exports.create = async (req, res) => {
     try {
@@ -10,6 +11,26 @@ module.exports.create = async (req, res) => {
         console.log(`Post created: ${post.id}`);
     } catch (err) {
         console.log(`Error Creating a Post: ${err}`);
+    }
+    return res.redirect("back");
+};
+module.exports.destroy = async (req, res) => {
+    try {
+        let post = await Post.findById(req.params.id);
+        if (post) {
+            if (post.user == req.user.id) {
+                await Post.deleteOne({_id: post._id});
+                console.log(`Deleted Post: ${req.params.id}`);
+                await Comment.deleteMany({post: req.params.id});
+                console.log(`Deleted Comments linked to Post: ${req.params.id}`);
+            } else {
+                console.log(`User(${req.user.id} is not authorized to delete Post(${post.id}))`);
+            }
+        } else {
+            console.log(`Post not found for ID: ${req.params.id}`);
+        }
+    } catch (err) {
+        console.log(`Error Deleting Post: ${err}`);
     }
     return res.redirect("back");
 };
