fe3bc8b5ae89d57969544cc82bf71af3810fab37
Madhav Prahlad <prahladmadhav@gmail.com>
2023-04-01 00:49:11 +0530

    Delete authorized comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,3 +22,23 @@ module.exports.create = async (req, res) => {
     }
     res.redirect("back");
 };
+module.exports.destroy = async (req, res) => {
+    try {
+        let comment = await Comment.findById(req.params.id);
+        if (comment) {
+            if (comment.user == req.user.id) {
+                await Comment.findByIdAndDelete(comment._id);
+                console.log(`Deleted Comment: ${req.params.id}`);
+                await Post.findByIdAndUpdate(comment.post, { $pull: { comments: comment._id } });
+                console.log(`Deleted Comment(${comment._id}) reference from Post(${comment.post})`);
+            } else {
+                console.log(`User(${req.user.id}) is not authorized to delete Comment(${comment.id})`);
+            }
+        } else {
+            console.log(`Comment not found for ID: ${req.param.id}`);
+        }
+    } catch (err) {
+        console.log(`Error Deleting comment: ${err}`);
+    }
+    res.redirect("back");
+};
